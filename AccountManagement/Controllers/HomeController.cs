863c09938165865e522588e665fcc748713f6b8b
Tala Naji <talanaji@gmail.com>
2013-02-13 11:19:58 +0200

    bug fixes

--- AccountManagement/Controllers/HomeController.cs
+++ AccountManagement/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AccountManagement.Domain.Model;
+using WebMatrix.WebData;
 
 namespace AccountManagement.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -15,11 +18,13 @@ namespace AccountManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Register(Account account)
         {
-            ViewBag.Message = "Your app description page.";
 
-            return View();
+            WebSecurity.CreateUserAndAccount(account.Email, account.Password);
+            
+            WebSecurity.Login(account.Email, account.Password);
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Contact()
